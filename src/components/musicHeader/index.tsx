d6cb4f3cbac85b75fc2b42fb15d99935c962ab72
lishanghang <630664429@qq.com>
2023-12-05 15:12:53 +0800

    update

--- src/components/musicHeader/index.tsx
+++ src/components/musicHeader/index.tsx
@@ -1,6 +1,8 @@
-import "./index.css";
+import { useState } from "react";
 import { SmileTwoTone, SearchOutlined } from "@ant-design/icons";
 import { Button, Input } from "antd";
+import { headerLinks } from "../../common/localData";
+import "./index.css";
 
 interface menuList {
   id: number;
@@ -9,76 +11,12 @@ interface menuList {
   children?: any;
 }
 
-//主页菜单数据
-const menuList: menuList[] = [
-  {
-    id: 1,
-    title: "发现音乐",
-    router: "",
-    children: [
-      {
-        title: "推荐",
-        router: "",
-      },
-      {
-        title: "排行榜",
-        router: "",
-      },
-      {
-        title: "歌单",
-        router: "",
-      },
-      {
-        title: "主播电台",
-        router: "",
-      },
-      {
-        title: "歌手",
-        router: "",
-      },
-      {
-        title: "新碟上架",
-        router: "",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "我的音乐",
-    router: "",
-  },
-  {
-    id: 3,
-    title: "关注",
-    router: "",
-  },
-  {
-    id: 4,
-    title: "商城",
-    router: "",
-  },
-  {
-    id: 5,
-    title: "音乐人",
-    router: "",
-  },
-  {
-    id: 6,
-    title: "云推歌",
-    router: "",
-  },
-  {
-    id: 7,
-    title: "下载客户端",
-    router: "",
-  },
-];
-
-const liList = menuList.map((item: menuList) => {
-  return <li key={item.id}>{item.title}</li>;
-});
-
 function MusicHeader() {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive((current) => !current);
+  };
+
   return (
     <div className="musicHeader">
       <div className="title">
@@ -86,14 +24,31 @@ function MusicHeader() {
         <h1>网易云音乐</h1>
       </div>
       <div className="menu">
-        <ul>{liList}</ul>
+        <ul>
+          {headerLinks.map((item) => {
+            return (
+              <li
+                onClick={handleClick}
+                key={item.title}
+                style={{
+                  backgroundColor: isActive ? "white" : "#242424",
+                }}
+              >
+                {item.title}
+              </li>
+            );
+          })}
+        </ul>
       </div>
       <div className="search">
-        <Input addonBefore={<SearchOutlined />} placeholder="large size" />
+        <Input
+          addonBefore={<SearchOutlined />}
+          placeholder="音乐/电台/视频/用户"
+        />
       </div>
       <div className="btns">
-        <Button>创作者中心</Button>
-        <Button>登录</Button>
+        <Button shape="round">创作者中心</Button>
+        <span>登录</span>
       </div>
     </div>
   );
